1247b7156fe911ca0f382d0c19810d903192b183
Braiton2117 <richarctm@gmail.com>
2024-11-20 00:29:59 -0500

    Update inventario.routes.js

--- src/routes/inventario.routes.js
+++ src/routes/inventario.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
+import multer from 'multer'
 import {getInventarios, getInventarioxid, postInventario, putInventario, patchInventario, deleteInventario} from '../controladores/inventarioCtrl.js'
+
+//configurar multer para almacenar las imagenes
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'uploads'); //carpeta donde se guardan las imagenes
+    },
+    filename:(req,file,cb)=>{
+        cb(null,`${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload=multer({storage});
 const router=Router()
 //armar nuestras rutas
 
